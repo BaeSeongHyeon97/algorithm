666fbb02906a5965d6a58661cd6dbde25195c6fe
bsh <tjdgus6937@naver.com>
2022-07-17 13:56:25 +0900

    7.17_2063

--- swea/2063_median.py
+++ swea/2063_median.py
@@ -1,2 +1,12 @@
 # 2063. 중앙값 찾기
 
+N = int(input())
+score = list(map(int,input().split()))
+
+score.sort()
+
+median = score[(N-1) // 2]
+
+print(median)
+
+# 7.17 나눗셈할때 (/) 쓰지말고 몫(//) / 나머지(%) 명확하게
