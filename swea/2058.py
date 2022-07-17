d51c075e8dd61d41b45d3cfcd29051b5d0a873bc
bsh <tjdgus6937@naver.com>
2022-07-17 13:45:40 +0900

    7.17_2058

--- swea/2058.py
+++ swea/2058.py
@@ -1,2 +1,23 @@
 # 2058. 자릿수 더하기
 
+# for 문
+num = int(input())
+sum = 0
+
+for i in range(1, 5) :
+    sum = sum + num % 10
+    num = num // 10
+
+print(sum)
+
+
+# while 문
+
+num = int(input())
+sum = 0
+
+while num != 0 :
+    sum = sum + num % 10
+    num = num // 10
+
+print(sum)
