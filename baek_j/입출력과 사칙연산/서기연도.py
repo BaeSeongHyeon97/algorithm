bfb5fc7e26449174b77f20cf4b59d95819e49d8a
배성현 <tjdgus6937@naver.com>
2022-07-23 19:57:30 +0900

    0723_print응용

---
+++ baek_j/입출력과 사칙연산/서기연도.py
@@ -0,0 +1,9 @@
+# 서기를 연도로 표시
+
+# 불기연도 -> 서기연도
+
+buda_year = int(input())
+
+year = buda_year - 543
+
+print(year)
---
+++ baek_j/입출력과 사칙연산/아이디중복확인.py
@@ -0,0 +1,6 @@
+# 아이디 중복확인
+# 중복이면 ??!출력
+
+same_id = input()
+
+print(same_id + '??!')
