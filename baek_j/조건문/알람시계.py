bb0a76459eb60f70f9768832474dbc868da409ca
배성현 <tjdgus6937@naver.com>
2022-07-26 23:36:11 +0900

    0726_조건문

---
+++ baek_j/조건문/알람시계.py
@@ -0,0 +1,26 @@
+# 알람시계
+# 45분일찍 알람설정
+
+H, M = map(int,input().split())
+alarm_H = 0
+alarm_M = 0
+
+if M >= 45:
+    alarm_H = H
+    alarm_M = M - 45
+elif 15 <= M < 45:
+    if H == 0:
+        alarm_H = H + 23
+        alarm_M = M - 15
+    else:
+        alarm_H = H - 1
+        alarm_M = M - 15
+elif 0 <= M < 15:
+    if H == 0:
+        alarm_H = H + 23
+        alarm_M = M + 15
+    else:
+        alarm_H = H - 1
+        alarm_M = M + 15
+
+print(alarm_H, alarm_M)
