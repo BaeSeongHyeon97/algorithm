15b64a38b8a06301805aba28f2d5b92173ac3aa4
배성현 <tjdgus6937@naver.com>
2022-08-02 11:11:31 +0900

    codeup_출력하기

---
+++ codeup100/출력하기.py
@@ -0,0 +1,24 @@
+# 01 
+print('Hello')
+
+# 02 
+print('Hello World')
+
+# 03
+print('Hello')
+print('World')
+
+# 04
+print("'Hello'")
+
+# 05
+print('"Hello World"')
+
+# 06
+print('\"!@#$%^&*()\'')
+
+# 07
+print('\"C:\\Download\\\'hello\'.py"')
+
+# 08
+print('print("Hello\\nWorld")')
