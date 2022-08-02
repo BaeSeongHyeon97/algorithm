ae5ecf5bd116e277ca981ef9fafe0242b57798cb
배성현 <tjdgus6937@naver.com>
2022-08-02 17:40:48 +0900

    0802_codeup_값변환

--- codeup100/값변환_25~31.py
+++ codeup100/값변환_25~31.py
@@ -20,10 +20,10 @@ a = input()
 n = int(a, 16)
 print('%o'%n)
 
-# 31 영문자 1개 10진수로 # ord() : 문자를 숫자로
+# 30 영문자 1개 10진수로 # ord() : 문자를 숫자로
 n = ord(input())
 print(n)
 
-# 32 10진수를 영문자로 # chr() : 숫자를 문자로
+# 31 10진수를 영문자로 # chr() : 숫자를 문자로
 n = int(input())
 print(chr(n))
