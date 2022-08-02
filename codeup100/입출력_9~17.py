7f28e5d27d2e25a814694edde5e58c854ea13dc3
배성현 <tjdgus6937@naver.com>
2022-08-02 16:09:25 +0900

    0802_codeup

---
+++ codeup100/입출력_9~17.py
@@ -0,0 +1,48 @@
+# 09
+word = input()
+print(word)
+
+# print(input())
+
+# 10
+num = int(input())
+print(num)
+
+# 11
+f = float(input())
+print(f)
+
+# 12
+a = int(input())
+b = int(input())
+
+print(a)
+print(b)
+
+# 13
+a = input()
+b = input()
+
+print(b)
+print(a)
+
+# 14 
+f = float(input())
+print(f)
+print(f)
+print(f)
+
+# 15
+a, b = map(int,input().split())
+
+print(a)
+print(b)
+
+# 16
+w1, w2 = input().split()
+print(w2, w1)
+
+# 17
+w = input()
+print(w, w, w)
+# 띄어쓰기해서 여러번 출력 ','를 이용해서 print하기
