f68090f293271d16e7269dbc0d051866c5d59a76
배성현 <tjdgus6937@naver.com>
2022-08-03 21:09:19 +0900

    0803_81

--- codeup100/81.py
+++ codeup100/81.py
@@ -7,3 +7,8 @@ for i in range(17):
    print('%X'%n, '*%X'%i, '=%X'%(n*i), sep='')
 
 # 16진수... 모르겠다..
+
+n=input()
+n=int(n,16)
+for i in range(1,16):
+    print('%X'%n, '*%X'%i, '=%X'%(n*i), sep='')
