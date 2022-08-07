dffe3d1de8518f0b39bdc08e364b6dd774d44ae7
배성현 <tjdgus6937@naver.com>
2022-08-08 00:24:06 +0900

    0808_설탕배달

---
+++ baek_j/설탕배달.py
@@ -0,0 +1,23 @@
+# 설탕가게에 N킬로그램 배달
+# 봉지는 3킬로 5킬로 
+# 최대한 적은 봉지
+# 봉지를 몇개 가져가야 할까
+
+
+N = int(input())
+cout = 0
+
+while N != 0:
+  if N % 5 == 0:
+    cout = cout + N // 5
+    N = N - 5 * (N // 5)
+  elif N == 1 or N == 2:
+      cout = -1
+      break
+  else:
+    N = N - 3
+    cout += 1
+
+
+print(cout)
+
